9a702ad60e6701699cddebe99e3827f034580d4d
BluNomad <jon@myrddin.digital>
2026-04-03 21:29:55 -0400

    Add build timestamp data attribute to root html

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_BUILD_TIMESTAMP: new Date().toISOString(),
+  },
   sassOptions: {},
   serverExternalPackages: ["better-sqlite3"],
   outputFileTracingIncludes: {
     "/api/posts": ["./data/nfc.db"],
     "/api/timestamps": ["./data/nfc.db"],
   },
-
 };
 
 export default nextConfig;
